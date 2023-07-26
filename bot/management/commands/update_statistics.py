5635066e489878f08e501d2bccf0c6bcd103172f
RomanRVV <rvachyov.roma@yandex.ru>
2023-07-27 02:41:26 +0300

    Add link statistics model

---
+++ bot/management/commands/update_statistics.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from bot.models import *
+import requests
+import argparse
+from urllib.parse import urlparse
+from BakeCake import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Обновляет статистику по кликам'
+    
+    def handle(self, *args, **kwargs):
+        def count_clicks(token, link):
+            bitlink_parse = urlparse(link)
+            bitlink = bitlink_parse._replace(scheme='').geturl()
+            url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+            headers = {
+                "Authorization": f"Bearer {token}"
+            }
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            clicks_count = response.json()['total_clicks']
+            return clicks_count
+
+        bitly_token = settings.bitly_token
+
+        while True:
+            links = LinkStatistics.objects.all()
+            for link in links:
+                clicks_count = count_clicks(bitly_token, link.title)
+                link.transitions = clicks_count
+                link.save()
