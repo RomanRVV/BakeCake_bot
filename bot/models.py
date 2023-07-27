da2b44c877c267ad3552b3dff68a1d9d7c2f1fc1
morozgit <morozec76@gmail.com>
2023-07-27 11:04:29 +0300

    Update models.py

--- bot/models.py
+++ bot/models.py
@@ -2,32 +2,46 @@
 from django.db import models
 
 
-class CakeBase(models.Model):
-    base_of_cake = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.base_of_cake}'
-
-
-class Level(models.Model):
-    num_of_level = models.IntegerField(max_value=3, min_value=1)
-    shape = models.ForeignKey(CakeBase)
-
-    def __str__(self):
-        return f'{self.num_of_level} уровневый {self.shape} формы'
-
-
-class CakeTopping(models.Model):
-    topping_for_cake = models.CharField(max_length=100, null=True)
-
 
 class Cake(models.Model):
-    prise = models.FloatField(max_digits=6, decimal_places=2)
-    level = models.ForeignKey(
-        Level,
-        on_delete=models.CASCADE)
-    cream = models.CharField(max_length=100)
-    topping = models.ForeignKey(CakeTopping, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='cakes/')
 
-    def __srt__(self):
-        return f'Торт{self.level} c {self.cream} и {self.topping} стоимостью {self.prise}'
+
+class CakeConstructor(models.Model):
+    CAKE_BASE_CHOICES = [
+        ('vanila', 'Ванильный бисквит'),
+        ('choco', 'Шоколадный бисквит'),
+        ('marble', 'Мраморный бисквит')
+    ]
+    TOPPING_CHOICES = [
+        ('wedge', 'Клиновый сироп'),
+        ('caramel', 'Карамельный сироп')
+    ]
+    LEVEL_CHOICES = [
+        ('one', '1 уровень'),
+        ('two', '2 уровня'),
+        ('three', '3 уровня')
+    ]
+    num_of_level = models.CharField(max_length=5, choices=LEVEL_CHOICES,
+                                    verbose_name='Количество уровней торта',
+                                    default='one')
+    base_of_cake = models.CharField(max_length=6,
+                                    choices=CAKE_BASE_CHOICES,
+                                    default='vanila',
+                                    verbose_name='Основа для торта')
+    topping = models.CharField(max_length=7,
+                               choices=TOPPING_CHOICES,
+                               blank=True,
+                               verbose_name='Топпинг')
+    blackberry = models.BooleanField(verbose_name='Еживика', default=False)
+    raspberry = models.BooleanField(verbose_name='Малина', default=False)
+    blueberry = models.BooleanField(verbose_name='Голубика', default=False)
+
+
+class LinkStatistics(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Bitly ссылка')
+    description = models.TextField(verbose_name='Описание ссылки')
+    transitions = models.IntegerField(verbose_name='Количество переходов по ссылке', default=0)
